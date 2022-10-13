cd4e770e7a9fdce26d3629b91255c9200580b634
Penn <pennilessfor@gmail.com>
2022-10-14 00:09:48 +0800

    添加mock插件

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,30 @@ import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import { resolve } from 'path'
 
+const mockPlugin = () => ({
+  name: 'mock-server',
+  configureServer(server){
+    // 返回一个在内部中间件安装后
+    // 被调用的后置钩子
+    return () => {
+      server.middlewares.use((req, res, next) => {
+        // 自定义请求处理...
+        console.log(req.url)
+        next()
+      })
+    }
+  }
+})
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     eslint({
       fix: true
-    })
+    }),
+    mockPlugin()
   ],
   resolve: {
     alias: {
