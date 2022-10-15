cb07364a2af76842c0903a6088bc7144eda2467c
Penn <pennilessfor@gmail.com>
2022-10-15 13:07:48 +0800

    完成mock功能

--- mock/index.ts
+++ mock/index.ts
@@ -1,23 +1,51 @@
-import {ViteDevServer} from "vite"
+import { ViteDevServer, Connect } from "vite"
+import * as http from 'node:http';
 
+type Service = [req: Connect.IncomingMessage, res: http.ServerResponse, next: Connect.NextFunction]
 
+type CallRoute = (
+  // s :Service
+  req: Connect.IncomingMessage,
+  res: http.ServerResponse,
+  next: Connect.NextFunction
+) => void
 
-// route 定义路由组数据
-const route = (route:string):string => {
-  const result = route + " | 404"
-  return result
+class Route {
+  service: Service
+  routerlist:CallRoute[] = []
+  any(route:string,func:CallRoute){
+    this.routerlist[route] = func
+  }
+  match(s:string){
+    let callback:CallRoute = this.routerlist[s]
+    if(callback == undefined){
+      this.service[1].end(s + " | 404")
+      return
+    }
+    callback(...this.service)
+  }
+  setService(s:Service){
+    this.service = s
+    this.match(s[0].url)
+  }
 }
 
+const router = new Route()
+router.any("/version", (req,res,next) =>{
+  res.end("0.0.1")
+})
+
+
+
+
+// 导出mock路由组
 export const mockPlugin = () => ({
   name: 'mock-server',
   configureServer(server:ViteDevServer){
     // 返回一个在内部中间件安装后
     // 被调用的后置钩子 return () => {}
-    server.middlewares.use("/mock", (req, res, next) => {
-      // console.log(req.url)
-      let result = route(req.url)
-      res.end(result)
+    server.middlewares.use("/mock", (...context) => {
+      router.setService(context)
     })
-    
   }
 })
