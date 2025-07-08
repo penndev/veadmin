915ec008c92e530ac7f0927ad9a8012d80a6953c
penn <pennilessfor@gmail.com>
2025-07-08 21:28:36 +0800

    fix: 移除truncated的title

--- src/components/ElTableColumnTruncated.vue
+++ src/components/ElTableColumnTruncated.vue
@@ -1,8 +1,8 @@
 <template>
   <el-table-column :label="label" :prop="prop" :align="align" v-bind="$attrs">
     <template #default="{ row }">
-      <el-tooltip :content="row[prop]">
-        <el-text truncated>{{ row[prop] }}</el-text>
+      <el-tooltip raw-content :content="'<pre>' + row[prop] + '</pre>'">
+        <el-text class="is-truncated">{{ row[prop] }}</el-text>
       </el-tooltip>
     </template>
   </el-table-column>
